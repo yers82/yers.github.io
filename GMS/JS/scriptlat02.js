718212d76c90d52179b5564a357237a84b3a4358
yers82 <yong.enth.rdjn.snts@gmail.com>
2023-09-18 19:13:26 +0700

    first commit

---
+++ GMS/JS/scriptlat02.js
@@ -0,0 +1,8 @@
+// Step 1 : Buat Toggles Class Active
+    const navbarnav = document.querySelector('navbar-nav');
+    document.querySelector('#hamburger-menu').onclick = () => {
+        navbarnav.classList.toggle('active');
+
+    };
+}
+
